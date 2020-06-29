1fe878b7421ee7622c9bc72529bc1bf16eebf3cc
Brian Nguyen <ngxtri@gmail.com>
2020-06-29 13:17:28 -0700

    php basic done WIP:install php mysql driver

---
+++ lnoapi/config/params.php
@@ -0,0 +1,8 @@
+<?php
+define('API_BASE', 'https://api.lnoapi/');
+return [
+    'adminEmail' => 'ngxtri@gmail.com',
+    'scraper_id' => 34,
+    'bsVersion' => 4,
+    'bsDependencyEnabled' => true
+];
