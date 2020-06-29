87cb02a4ec50b561c9364bf33cd7dec7844383b6
Brian Nguyen <ngxtri@gmail.com>
2020-06-28 21:52:00 -0700

    telnet done WIP: copy file to variable

---
+++ lnoapi/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=usvsolutions.com;dbname=lno',
+    'username' => 'lno',
+    'password' => 'lTrapok)1',
+    'charset' => 'utf8',
+];
